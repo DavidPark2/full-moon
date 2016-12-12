d399a9c8d9aa63c793595ad636c3d225b2af03b5
david <dpark543107@gmail.com>
2016-12-12 16:06:04 -0600

    separated app.js

---
+++ app/app.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.
+  module('myApp').
+  config(['$locationProvider', '$routeProvider',
+    function($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+      when('/view1', {
+        template: '<view1></view1>'
+      }).
+      otherwise('/view1');
+    }
+  ]);
